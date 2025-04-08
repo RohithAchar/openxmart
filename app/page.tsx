eb7d58200cc5d58c442d8596c41b6e9f8378207f
RohithAchar <rohithachar2000@gmail.com>
2025-04-08 10:16:43 +0530

    marketing color

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Marketing from "@/components/marketing";
 
 export default function Home() {
   return (
-    <div className="max-w-screen-xl mx-auto">
+    <div className="p-4 sm:p-6 lg:p-8 space-y-4 sm:space-y-6 lg:space-y-8">
       <Billboard />
-      <div>I dont get this section!!!!!!!</div>
+      {/* <div>I dont get this section!!!!!!!</div> */}
       <Marketing />
     </div>
   );
--- components/marketing.tsx
+++ components/marketing.tsx
@@ -1,35 +1,38 @@
+import {
+  Handshake,
+  ShieldCheck,
+  MapPinCheck,
+  ChartNoAxesCombined,
+} from "lucide-react";
+
 const Marketing = () => {
   return (
-    <div className="flex justify-center gap-8 md:gap-24 flex-wrap px-4 py-8">
+    <div className="grid grid-cols-2 md:grid-cols-4 py-8 gap-4 gap-y-8 rounded-2xl bg-primary text-primary-foreground">
       <div className="overflow-hidden text-center flex flex-col items-center">
-        <img
-          src="/images/trust.png"
-          alt="trust"
-          className="w-20 h-20 md:w-24 md:h-24 object-cover"
+        <Handshake
+          strokeWidth={1.3}
+          className="w-12 h-12 md:w-24 md:h-24 object-cover"
         />
         <h2 className="text-xl font-bold mt-4">100% Trust</h2>
       </div>
       <div className="overflow-hidden text-center flex flex-col items-center">
-        <img
-          src="/images/secure.png"
-          alt="trust"
-          className="w-20 h-20 md:w-24 md:h-24 object-cover"
+        <ShieldCheck
+          strokeWidth={1.3}
+          className="w-12 h-12 md:w-24 md:h-24 object-cover"
         />
         <h2 className="text-xl font-bold mt-4">Secure Payment</h2>
       </div>
       <div className="overflow-hidden text-center flex flex-col items-center">
-        <img
-          src="/images/india.png"
-          alt="trust"
-          className="w-20 h-20 md:w-24 md:h-24 object-cover"
+        <MapPinCheck
+          strokeWidth={1.3}
+          className="w-12 h-12 md:w-24 md:h-24 object-cover"
         />
         <h2 className="text-xl font-bold mt-4">Made for Bharath</h2>
       </div>
       <div className="overflow-hidden text-center flex flex-col items-center">
-        <img
-          src="/images/diagram.png"
-          alt="trust"
-          className="w-20 h-20 md:w-24 md:h-24 object-cover"
+        <ChartNoAxesCombined
+          strokeWidth={1.3}
+          className="w-12 h-12 md:w-24 md:h-24 object-cover"
         />
         <h2 className="text-xl font-bold mt-4">Trust</h2>
       </div>
