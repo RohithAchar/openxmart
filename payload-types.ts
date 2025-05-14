47ab919808c6596c9fbc4d512e36aca62487f912
Rohith Achar <rohithachar2000@gmail.com>
2025-05-14 16:10:53 +0530

    supabase

--- payload-types.ts
+++ payload-types.ts
@@ -146,6 +146,7 @@ export interface User {
 export interface Media {
   id: string;
   alt: string;
+  prefix?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -273,6 +274,7 @@ export interface UsersSelect<T extends boolean = true> {
  */
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
+  prefix?: T;
   updatedAt?: T;
   createdAt?: T;
   url?: T;
